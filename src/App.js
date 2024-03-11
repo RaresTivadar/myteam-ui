e648d7f0b10b6f37c35f722566396dbace710ae5
RaresTivadar <78786313+RaresTivadar@users.noreply.github.com>
2024-03-11 19:37:28 +0200

    MYT-3: Update homepage layout and integrate routing in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './pages/HomePage';
+import SignUpPage from './pages/SignUpPage';
 import './App.css';
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<HomePage />} /> {/* Use element prop instead of component */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<SignUpPage />} />
         </Routes>
       </div>
     </Router>
